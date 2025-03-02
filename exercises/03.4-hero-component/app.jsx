d96ae4b1bb68402e35ec88289fe2429190e7007b
JosuVentu04 <venturajosue711@gmail.com>
2025-03-02 23:04:12 +0000

    Ejercicios terminados

--- exercises/03.4-hero-component/app.jsx
+++ exercises/03.4-hero-component/app.jsx
@@ -1,23 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 const Hero = (props) => {
-  // Here you have to return expected html using the properties being passed to the component
+	// Here you have to return expected html using the properties being passed to the component
+	return (
+		<div className="Presentation">
+			<div className="presentation-body">
+				<h2 className="Presentation-title">{props.title}</h2>
+				<p className="-Presentation-Description">{props.description}</p>
+				<a href={props.buttonURL} className="btn btn-primary">
+					{props.buttonLabel}
+				</a>
+			</div>
+		</div>
+	);
 };
 
 Hero.propTypes = {
-  // PropTypes here
-  title: PropTypes.string,
+	// PropTypes here
+	title: PropTypes.string,
+	description: PropTypes.string,
+	buttonLabel: PropTypes.string,
+	buttonURL: PropTypes.string,
 };
 
 ReactDOM.render(
-  <Hero
-    title="Welcome to react"
-    description="React is the most popular rendering library in the world"
-    buttonLabel="Go to the official website"
-    buttonURL="https://reactjs.org/"
-  />,
+	<Hero
+		title="Welcome to react"
+		description="React is the most popular rendering library in the world"
+		buttonLabel="Go to the official website"
+		buttonURL="https://reactjs.org/"
+	/>,
 
-  document.querySelector('#myDiv')
+	document.querySelector("#myDiv")
 );
